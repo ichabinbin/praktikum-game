7fab52052ab7cda8d4a87a65b6c32db5da3e7104
ichabinbin <60200112011@uin-alauddin.ac.id>
2015-05-06 02:43:43 +0800

    lap1

---
+++ Assets/script.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class script: MonoBehaviour {
+
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+
+			GetComponent<Rigidbody> ().velocity = Vector3.zero;
+			GetComponent<Rigidbody> ().AddForce (Vector3.up*200);
+		}
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			GetComponent<Rigidbody> ().velocity = Vector3.zero;
+			GetComponent<Rigidbody> ().AddForce (Vector3.right*100);
+		}
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			GetComponent<Rigidbody> ().velocity = Vector3.zero;
+			GetComponent<Rigidbody> ().AddForce (Vector3.left*100);
+		}
+	}
+}
